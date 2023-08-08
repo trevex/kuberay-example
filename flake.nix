163642fce377082057725a82d0f9e69f6cbea1a7
Niklas Voss <niklas.voss@gmail.com>
2023-08-08 10:45:41 +0200

    Add basic nix direnv for development

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "kube-ray-example";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      overlays = [
+        (final: prev: {
+          # No overlays for now...
+        })
+      ];
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        devShell = pkgs.mkShell rec {
+          name = "kube-ray-example";
+
+          buildInputs = with pkgs; [
+            gnumake
+            findutils
+            tmux
+            google-cloud-sdk
+            (google-cloud-sdk.withExtraComponents [
+              pkgs.google-cloud-sdk.components.kubectl
+              pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
+            ])
+            terraform
+            tfsec
+            tflint
+          ];
+        };
+      }
+    );
+}
